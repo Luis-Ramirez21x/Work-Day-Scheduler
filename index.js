190ee1b024d0602e3e98b00130ae81872473c5bb
Luis Ramirez <luis21x@outlook.com>
2021-09-27 14:41:23 -0400

    added time and colored blocked based on past present or future. I hve also added a button for each row. i need to make it function and save whatever value is in the corresponding div

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 var dayDisplayEl= $('#currentDay');
 var plannerEl= $('.container');
-var hourShown =9;
 
 var date = moment().format('dddd, MMM Do');
 $(dayDisplayEl).text(date);
 console.log(date);
 
+
+var currentTime = moment().format('H');
+//console.log(currentTime);
+var adjCurrentTime = currentTime - 12;
+//console.log(adjCurrentTime);
+
+
+
 function createTimeBlocks(){
 for(i = 0; i< 9; i++){
     var divEl = $('<div>');
-    var btnEl = $('<button>');
     var hourEL = $('<div>');
-    var textEl = $('<div>');
-    var saveBtnEl = $('<div>');
+    var textEl = $('<input>');
+    var saveBtnEl = $('<button>');
     divEl.addClass('row');
     divEl.addClass('time-block')
     hourEL.addClass('hour');
-    textEl.addClass('past');
-    saveBtnEl.addClass('saveBtn');
+    //textEl.addClass('past');
+    textEl.attr("data-index", i);
+    saveBtnEl.addClass('saveBtn').text('SAVE');
+    
     plannerEl.append(divEl);
     divEl.append(hourEL);
     divEl.append(textEl);
@@ -25,9 +33,34 @@ for(i = 0; i< 9; i++){
 
     if(i <4){
     hourEL.text(i+9);
+        var hour = parseInt(hourEL.text());
+        console.log(hour);
+        if(hour < currentTime){
+            textEl.addClass('past');
+        } else if(hour == currentTime){
+            textEl.addClass('present');
+        } else{
+            textEl.addClass('future');
+        }
+       
     } else{
         hourEL.text(i-3);
+        var hour = parseInt(hourEL.text());
+        console.log(hour);
+        if(hour < adjCurrentTime){
+            textEl.addClass('past');
+        } else if(hour == adjCurrentTime){
+            textEl.addClass('present');
+        } else{
+            textEl.addClass('future');
+        }
+       
+        
     }
+
+    
+
+
 
     
 }
@@ -35,4 +68,25 @@ for(i = 0; i< 9; i++){
 
 
 
+
+function saveToLocal(){
+    console.log(date);
+}
+
 createTimeBlocks();
+var saveBtn = $('.saveBtn');
+plannerEl.on('click', function(){
+    var id = $(this).attr('data-index');
+    console.log(id);
+    
+    
+
+
+    console.log(element);
+})
+/*
+var element = event.target;
+if(element.matches('button') === true){
+    var index = element.parentElement.getAttribute("data-index");
+    console.log(index);
+}*/
