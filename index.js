585e0ea1abc0af82f580ac00e06b0123677cf6b6
Luis Ramirez <luis21x@outlook.com>
2021-09-27 12:45:51 -0400

    added date to top of page and added function to genereate timeblocks from 9-5 along with the coresponding times

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
-var daydisplay = $('#currentDay');
-var container = $('.container');
+var dayDisplayEl= $('#currentDay');
+var plannerEl= $('.container');
+var hourShown =9;
 
+var date = moment().format('dddd, MMM Do');
+$(dayDisplayEl).text(date);
+console.log(date);
 
-function displayCurrentDay(){
+function createTimeBlocks(){
+for(i = 0; i< 9; i++){
+    var divEl = $('<div>');
+    var btnEl = $('<button>');
+    var hourEL = $('<div>');
+    var textEl = $('<div>');
+    var saveBtnEl = $('<div>');
+    divEl.addClass('row');
+    divEl.addClass('time-block')
+    hourEL.addClass('hour');
+    textEl.addClass('past');
+    saveBtnEl.addClass('saveBtn');
+    plannerEl.append(divEl);
+    divEl.append(hourEL);
+    divEl.append(textEl);
+    divEl.append(saveBtnEl);
 
+    if(i <4){
+    hourEL.text(i+9);
+    } else{
+        hourEL.text(i-3);
+    }
+
+    
+}
 }
 
 
+
+createTimeBlocks();
